e9e4388796b6b29d54ef3a89a7ec6737c1a5b928
Rene-Ngong-N <renengong16@gmail.com>
2023-01-19 22:14:27 -0500

    Testing each value for service account

--- terraform/var.tf
+++ terraform/var.tf
@@ -1,9 +1,9 @@
-# variable "name" {
-#     description = "name to used"
-#     type = string
-#     default = "gitops"
+variable "name" {
+    description = "name to used"
+    type = string
+    default = "gitops"
   
-# }
+}
 
 variable "project_id" {
     description = "project id"
